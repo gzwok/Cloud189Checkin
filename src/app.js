c92a35531f09c159bba0ccacf2b0b79997927da4
gzwok <386861643@qq.com>
2025-01-20 17:47:31 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -49,7 +49,7 @@ const doTask = async (cloudClient) => {
  // const res2 = await cloudClient.taskSign();
  // buildTaskResult(res2, result);
 
-  await delay(5000); // 延迟5秒
+ // await delay(5000); // 延迟5秒
   //const res3 = await cloudClient.taskPhoto();
   //buildTaskResult(res3, result);
 
@@ -199,7 +199,7 @@ async function main() {
     if (userName && password) {
       const userNameInfo = mask(userName, 3, 7);
       try {
-        logger.log(`${number}`+"."+`账户 ${userNameInfo}开始执行`);
+        logger.log(`${number}`+".  "+`账户 ${userNameInfo}开始执行`);
         const cloudClient = new CloudClient(userName, password);
         await cloudClient.login();
         const result = await doTask(cloudClient);
@@ -228,7 +228,7 @@ async function main() {
           throw e;
         }
       } finally {
-        logger.log(`账户 ${userNameInfo}执行完毕-------------`);
+        logger.log(`账户 ${userNameInfo}执行完毕---------------`);
       }
     }
   }
